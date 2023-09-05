376fb3378bb7698a0b3147da7edc6ed3e2d14809
godsadeser <ducmanh0801@gmail.com>
2023-09-05 19:26:48 +0700

    finish dialog and read form

--- asset/js/script.js
+++ asset/js/script.js
@@ -12,11 +12,12 @@ function book(name, author, page, read){
 let openFormBtn = document.querySelector('#open-form')
 let loginBtn = document.getElementById("login-btn")
 let displayBtn = document.querySelectorAll('#dp-btn')
-let openRemoveBtn = document.querySelectorAll('#open-remove')
+let removeDialog = document.querySelectorAll('#remove-dialog')
 let updateBtn = document.querySelectorAll('#upd-btn')
 
     //On dialog
 let addDialog = document.getElementById("add-dialog")
+let addForm = document.getElementById("add-form")
 let removeCf = document.getElementById("rm-dialog")
 let addBtn = document.getElementById("add-btn")
 let closeBtn = document.querySelectorAll("#close-btn")
@@ -29,6 +30,7 @@ let container = document.querySelector(".container")
 let nameInput = document.getElementById("name")
 let authorInput = document.getElementById("author")
 let pageInput = document.getElementById("page")
+let readStatus = document.querySelectorAll('input[name="readStatus"]')
 
 
 //Function to fill a form to add new Object to the Array
@@ -57,16 +59,43 @@ function getRandomColor() {
 //Into action: Books are displayed in container with grid system
 
     //Read click event - Open form - Close form
+let isDialogOpen = false
 openFormBtn.addEventListener("click", ()=>{
     addDialog.showModal()
+    isDialogOpen = true
 })
-closeBtn.forEach(btn => btn.addEventListener("click", (modal)=> {
-    modal.close("canceled")
+closeBtn.forEach(btn => btn.addEventListener("click", ()=> {
+    addForm.removeAttribute("novalidate")
+    addDialog.close("canceled")
+    addForm.setAttribute("novalidate","true")
+    isDialogOpen = false
 }))
     //Read form input then assign values to create new object
-
+function checkStatus(){//To check whether the book is read or not read
+    let result = ""
+    for(const status of readStatus){
+        if(status.checked){
+            result = status.value
+        }
+    }
+    return result
+}
+addBtn.addEventListener('click', (e)=> {
+    if(isDialogOpen == true && !addForm.checkValidity()){
+        e.preventDefault()
+    }
+    add(nameInput.value, authorInput.value, pageInput.value, checkStatus())
+    modal.close("canceled")
+})
     //Loop through array to display everything as cards
+function display(){
+    library.forEach(book => {
+        let card = document.createElement('div[class="book-card"')
+        
 
+        
+    });
+}
     //Read click event - display each book's card
 
     //Read click event - remove 
